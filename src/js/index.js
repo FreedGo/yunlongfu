3395e843cd60d0b570f875b0bb5638314d0faea0
Freed <flyxz@126.com>
2017-03-06 17:30:48 +0800

    主要结构确定

---
+++ src/js/index.js
@@ -0,0 +1,28 @@
+/**
+ * Created by Freed on 2017/3/6.
+ * E-mail:flyxz@126.com.
+ * GitHub:FreedGo@github.com.
+ */
+var areaLists = new Vue({
+	el:'#area-lists',
+	data:{
+		todos:[
+			{title:'浦东新区',id:'1212',num:1547878},
+			{title:'黄浦区',id:'12123',num:1547878},
+			{title:'静安区',id:'1212',num:1547878},
+			{title:'徐汇区',id:'1212',num:1547878},
+			{title:'长宁区',id:'1212',num:1547878},
+			{title:'虹口区',id:'1212',num:1547878},
+			{title:'杨浦区',id:'1212',num:1547878},
+			{title:'闸北区',id:'1212',num:1547878},
+			{title:'普陀区',id:'1212',num:1547878},
+			{title:'宝山区',id:'1212',num:1547878},
+			{title:'闵行区',id:'1212',num:1547878},
+			{title:'嘉定区',id:'1212',num:1547878},
+			{title:'松江区',id:'1212',num:1547878},
+			{title:'青浦区',id:'1212',num:1547878},
+			{title:'金山区',id:'1212',num:1547878},
+			{title:'崇明区',id:'1212',num:1547878}
+		]
+}
+})
